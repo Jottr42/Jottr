c5c2235b447fd0fd13a0f348628ea83d87e62ed0
Sami Messai <messai.sami@gmail.com>
2023-01-31 09:26:39 -0600

    first commit

--- client/App.jsx
+++ client/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './containers/MainPage.jsx';
 import LandingPage from './containers/LandingPage.jsx';
+import Navbar from './components'
 //importing styling and images
 import './stylesheets/styles.scss';
 
