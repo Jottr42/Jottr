bffefe1cf02016c344624be4b3a716a712c1847c
Amrit Ramos <amrit.ramos@gmail.com>
2023-01-31 18:42:42 -0300

    more fixes to server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -64,11 +64,11 @@ userController.verifyUser = async (req, res, next) => {
 
 userController.getUser = async (req, res, next) => {
   console.log(`req.params in GET USER======`, req.params);
-  const { item_id } = req.params;
-  const insertArray = [item_id];
+  const { user_id } = req.params;
+  const insertArray = [user_id];
 
   try {
-    const sqlQuery = `SELECT * FROM users where item_id = $1`;
+    const sqlQuery = `SELECT * FROM users where user_id = $1`;
     const result = await db.query(sqlQuery, insertArray);
     console.log(`result======`, result);
     res.locals.userInfo = result.rows[0];
